c3259c45abf9db82f5554a0b9257f558117d3cce
Niko Ekberg <niko.ekberg@gmail.com>
2015-03-28 00:08:37 +0200

    Uusia luokkia

---
+++ BlackJack/src/main/java/nikoe/blackjack/logic/BlackJackGameManager.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nikoe.blackjack.logic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import nikoe.blackjack.logic.players.Dealer;
+import nikoe.blackjack.logic.players.Human;
+import nikoe.blackjack.util.PropertyReader;
+
+/**
+ *
+ * @author Niko
+ */
+public class BlackJackGameManager {
+    
+    private List<Seat> seats;
+    private Dealer dealer;
+    private PropertyReader props;
+    
+    public BlackJackGameManager() {
+        this.seats = new ArrayList<>();
+        this.dealer = new Dealer("Dealer");
+        this.props = new PropertyReader("game.properties");
+        initSeats();
+    }
+    
+    private void initSeats() {
+        int maxSeatAmount = Integer.parseInt(this.props.getProperty("table.numberOfSeats", "6"));
+        if(maxSeatAmount > 6) {
+            maxSeatAmount = 6;
+        }
+        for(int i = 1; i <= maxSeatAmount; i++) {
+            this.seats.add(new Seat(i));
+        }
+    }
+    
+    public void addPlayerToSeat(String playerName, int seatNumber) {
+        if(seatExists(seatNumber)) {
+            for(Seat seat : this.seats) {
+                if(seat.getSeatNumber() == seatNumber) {
+                    seat.setPlayer(new Human(playerName));
+                }
+            }
+        } 
+    }
+    
+    private boolean seatExists(int seatNumber) {
+        if(seatNumber > this.seats.size()) {
+            return false;
+        }
+        if(seatNumber < 0) {
+            return false;
+        }
+        
+        return true;
+    }
+            
+    public Seat getSeat(int seatNumber) {
+        if(!seatExists(seatNumber)) {
+            return null;
+        }
+        return this.seats.get(seatNumber);
+    }
+    
+    public List<Seat> getSeats() {
+        return Collections.unmodifiableList(seats);
+    }
+    
+    public void releaseSeat(int seatNumber) {
+        if(seatExists(seatNumber)) {
+            for(Seat seat : this.seats) {
+                if(seat.getSeatNumber() == seatNumber) {
+                    seat.release();
+                }
+            }
+        }
+    }
+}
